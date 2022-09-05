d6566e4fdb9c828b7931f3be677131daf1c72bf8
zz <1871490331@qq.com>
2022-09-05 11:41:18 +0800

    完成布局

---
+++ pages/home/home.vue
@@ -0,0 +1,34 @@
+<template>
+  <view>
+    <view class="top-search"><view class="search">搜索</view></view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss">
+.top-search {
+  width: 100%;
+  height: 200rpx;
+  background-color: #c00000;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  padding-bottom: 20px;
+  .search {
+    width: 60%;
+    height: 70rpx;
+    background-color: #fff;
+    border-radius: 20rpx;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
